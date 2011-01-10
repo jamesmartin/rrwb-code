f33357c877f98f5039a757c31ca4ec16bd931f36
James Martin <jimmymartin@gmail.com>
2011-01-10 21:00:03 +1100

    Refactored long method in Report by extracting methods

--- robot/machine_spec.rb
+++ robot/machine_spec.rb
@@ -1,3 +1,4 @@
+$: << File.expand_path(File.dirname(__FILE__))
 require 'machine'
 
 describe Machine do
--- robot/report.rb
+++ robot/report.rb
@@ -1,18 +1,38 @@
 class Report
   def Report.report(out, machines, robot)
+    write_factory_report_header_to(out)
+    write_machine_report_to(out, machines)
+    write_robot_location_to(out, robot)
+    write_report_footer_to(out)
+  end
+
+  def Report.write_factory_report_header_to(out)
     out.print "FACTORY REPORT\n"
+  end
+
+  def Report.write_machine_report_to(out, machines)
     machines.each do |machine|
       out.print "Machine #{machine.name}"
       out.print " bin=#{machine.bin}" if machine.bin != nil
       out.print "\n"
     end
+    write_line_break_to(out)
+  end
+
+  def Report.write_line_break_to(out)
     out.print "\n"
+  end
+  
+  def Report.write_robot_location_to(out, robot)
     out.print "Robot"
     if robot.location != nil
       out.print " location=#{robot.location.name}"
     end
     out.print " bin=#{robot.bin}" if robot.bin != nil
-    out.print "\n"
+    write_line_break_to(out)
+  end
+
+  def Report.write_report_footer_to(out)
     out.print "========\n"
   end
 end
